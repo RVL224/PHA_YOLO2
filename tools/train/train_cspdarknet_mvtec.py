f4eb878e69c72e2467b7fb2adbc3fda267588942
Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>
2025-06-28 01:39:28 +0800

    Update train_cspdarknet_mvtec.py
    Signed-off-by: Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>

--- tools/train/train_cspdarknet_mvtec.py
+++ tools/train/train_cspdarknet_mvtec.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
  
 def main():
-    # model = YOLO('/workspace/yolov10/ultralytics/cfg/models/cspdarknet/.yaml')
-    model.train(data='/workspace/yolov10/ultralytics/cfg/datasets/MVTEC.yaml', epochs=300, imgsz=640, batch=64,device=[0,1,2,3] , name="x_mvtec" )
+    # model = YOLO('/ultralytics/cfg/models/cspdarknet/.yaml')
+    model.train(data='/ultralytics/cfg/datasets/MVTEC.yaml', epochs=100, imgsz=640,  amp=False, optimizer='SGD', batch=64,device=[0,1,2,3] , name="x_mvtec" )
 if __name__ == '__main__':
     main()

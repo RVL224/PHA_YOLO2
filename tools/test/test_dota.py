7928b934253aa7af0bd3eb68c4cb124b235a4487
Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>
2025-06-28 01:41:52 +0800

    Update test_dota.py
    Signed-off-by: Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>

--- tools/test/test_dota.py
+++ tools/test/test_dota.py
@@ -2,8 +2,11 @@ from ultralytics import YOLO
 import numpy as np
 
 # Load a model
-model = YOLO('/workspace/PHA_YOLO/runs/obb/       /weights/best.pt')
+model = YOLO('/ultralytics/runs/obb/       /weights/best.pt')
 metrics = model.val(
-  data='/workspace/PHA_YOLO/ultralytics/cfg/datasets/DOTA.yaml',
-  split='test'
+  data='/ultralytics/cfg/datasets/DOTA.yaml',
+  split='test',
+  task="obb",
+  conf=0.01,
+  nms=True,
 )

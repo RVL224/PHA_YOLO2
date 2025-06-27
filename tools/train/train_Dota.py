394a6af0fd85ae12f87f67c6bce9c901eea9246e
Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>
2025-06-28 01:39:08 +0800

    Update train_Dota.py
    Signed-off-by: Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>

--- tools/train/train_Dota.py
+++ tools/train/train_Dota.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
  
 def main():
-    model = YOLO('/workspace/PHA_YOLO/ultralytics/cfg/models/v9/yolov9c_PHA-uvc.yaml', task="obb").load('/workspace/PHA_YOLO/yolov9c.pt')  # build from YAML and transfer weights
-    model.train(data='/workspace/PHA_YOLO/ultralytics/cfg/datasets/DOTA.yaml', epochs=300, imgsz=640, batch=4, device=0,name="PHA-YOLO_DOTA")
+    model = YOLO('/ultralytics/cfg/models/v9/yolov9c_PHA-uvc.yaml', task="obb").load('/workspace/PHA_YOLO/yolov9c.pt')  # build from YAML and transfer weights
+    model.train(data='/ultralytics/cfg/datasets/DOTA.yaml', epochs=100, imgsz=640, batch=4, device=[0,1,2,3], amp=False, optimizer='SGD', name="PHA-YOLO_DOTA")
 if __name__ == '__main__':
     main()

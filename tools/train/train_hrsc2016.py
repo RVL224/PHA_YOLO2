dbdb36f7fd6425c016ad325463fdddabfef2b5fc
Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>
2025-06-28 01:39:56 +0800

    Update train_hrsc2016.py
    Signed-off-by: Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>

--- tools/train/train_hrsc2016.py
+++ tools/train/train_hrsc2016.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
  
 def main():
-    model = YOLO('/workspace/PHA_YOLO/ultralytics/cfg/models/v9/yolov9c_PHA.yaml', task="obb").load('/workspace/PHA_YOLO/yolov9c.pt')  # build from YAML and transfer weights
-    model.train(data='/workspace/PHA_YOLO/ultralytics/cfg/datasets/hrsc.yaml', epochs=300, imgsz=640, batch=16, device=0,name="PHA-YOLO_hrsc2016")
+    model = YOLO('/ultralytics/cfg/models/v9/yolov9c_PHA.yaml', task="obb").load('yolov9c.pt')  # build from YAML and transfer weights
+    model.train(data='/ultralytics/cfg/datasets/hrsc.yaml', epochs=100, imgsz=640, batch=16, device=[0,1,2,3], amp=False, optimizer='SGD', name="PHA-YOLO_hrsc2016")
 if __name__ == '__main__':
     main()
     

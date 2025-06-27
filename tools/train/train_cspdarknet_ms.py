e7e6a89efd825e40a4a52a82686fe6bd0bb95a9c
Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>
2025-06-28 01:38:35 +0800

    Update train_cspdarknet_ms.py
    Signed-off-by: Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>

--- tools/train/train_cspdarknet_ms.py
+++ tools/train/train_cspdarknet_ms.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
  
 def main():
-    # model = YOLO('/workspace/yolov10/ultralytics/cfg/models/cspdarknet/.yaml')
-    model.train(data='/workspace/yolov10/ultralytics/cfg/datasets/HRSC2016_MS.yaml', epochs=300, imgsz=640, batch=64,device=[0,1,2,3] , name="x_ms" )
+    # model = YOLO('/ultralytics/cfg/models/cspdarknet/.yaml')
+    model.train(data='/ultralytics/cfg/datasets/HRSC2016_MS.yaml', epochs=100, imgsz=640, batch=64,device=[0,1,2,3] , amp=False, optimizer='SGD', name="x_ms" )
 if __name__ == '__main__':
     main()

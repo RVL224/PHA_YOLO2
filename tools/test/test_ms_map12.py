7a7b04c823c457eb9b18cd92bae690c0ee329e86
Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>
2025-06-28 20:51:41 +0800

    Update test_ms_map12.py
    Signed-off-by: Huan-Wei Hsu <139125353+Wei0209@users.noreply.github.com>

--- tools/test/test_ms_map12.py
+++ tools/test/test_ms_map12.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import numpy as np
 
 # Load a model
-model = YOLO('/ultralytics//runs/obb/       /weights/best.pt')
+model = YOLO('/ultralytics/runs/obb/       /weights/best.pt')
 metrics = model.val(
     data='/ultralytics/cfg/datasets/HRSC2016_MS.yaml',
     split='test',
